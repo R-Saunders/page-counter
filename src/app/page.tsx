690424415973a1c0dd3b6bf0e69cea7b42285db0
R-Saunders <richsaunders@protonmail.com>
2023-08-17 14:10:29 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
-  )
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center text-9xl p-24">
+			<p>0</p>
+		</main>
+	);
 }
